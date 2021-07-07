a2602f7e3c54fb5dc421fa058228a593992e7741
Abrar <abrar.nitk@gmail.com>
2021-07-07 07:16:24 +0530

    project testing message

---
+++ node-api/app.js
@@ -0,0 +1,26 @@
+const { TwitterApi } = require('twitter-api-v2');
+const secrets = require('./secrets.json')
+
+
+console.log(secrets)
+
+const twitterClient = new TwitterApi({
+   appKey: secrets.appKey,
+   appSecret: secrets.appSecret,
+   accessToken: secrets.accessToken,
+   accessSecret: secrets.accessSecret,
+ });
+
+
+async function getUser() {
+    //const user = await twitterClient.v2.userByUsername('naval');
+    //console.log(user)
+    //const followers = await twitterClient.v2.followers(user.data.id);
+    //console.log(followers);
+
+    await twitterClient.v1.tweet('Hello, this is a test. \n @wilderbit');
+
+    // await twitterClient.v1.uploadMedia(await fs.promises.readFile(path), { type: 'jpg' })
+}
+
+getUser();
